177dd4b39acb8fc9fc1280a8c4bab12a87f0db9b
seufster71 <seufster@gmail.com>
2022-05-18 16:35:47 -0400

    more setup

---
+++ logout-view.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text, View } from 'react-native';
+
+
+export default function LogoutView({currentState, fields, texts, labels, onChangeLogin,
+  onChangeRegistration, fieldChangeEvent, onForgotPassword,
+  fieldBlurEvent, buttonClick, handleChange, changeView }) {
+
+
+    return (
+	 <View style={styles.container}>
+      <Text>Logout Page</Text>
+    </View>
+    );
+}
+
+
+LogoutView.propTypes = {
+  currentState: PropTypes.object,
+	fields: PropTypes.object,
+  texts: PropTypes.object,
+  labels: PropTypes.object,
+  onChangeLogin: PropTypes.func,
+  onChangeRegistration: PropTypes.func,
+  onForgotPassword: PropTypes.func,
+  fieldChangeEvent: PropTypes.func,
+  fieldBlurEvent: PropTypes.func,
+  buttonClick: PropTypes.func,
+  handleChange: PropTypes.func,
+  changeView: PropTypes.func
+};
